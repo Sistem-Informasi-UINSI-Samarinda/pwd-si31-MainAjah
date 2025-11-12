5e1a937aa09d01d6ba7423e13ba754e2f9a171ca
MainAja <rivanipani545@gmail.com>
2025-11-12 15:02:44 +0800

    praktikum 1

---
+++ config/koneksi.php
@@ -0,0 +1,16 @@
+<?php
+$servername ="localhost";
+$username ="root";
+$password ="";
+$database ="pwd_2441919037";
+
+
+$conn = mysqli_connect($servername, $username, $password, $database);
+
+if(!$conn){
+    die("Koneksi Gagal:". mysqli_connect_error());
+}
+
+echo "Koneksi Berhasil";
+mysqli_close($conn);
+?>
